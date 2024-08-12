99d8b49aae8cc6678abf9bae7695b6946cfee80f
Nerdman106 <50927056+Nerdman106@users.noreply.github.com>
2024-08-12 16:37:51 +0300

    Rock Paper Scissors with GUI

--- script/index.js
+++ script/index.js
@@ -4,22 +4,14 @@ let global = {
 }
 
 const setup = () => {
+    let rock = document.getElementById("rock")
+    let paper = document.getElementById("paper")
+    let scissors = document.getElementById("scissors")
 
-
-    let rounds = 0;
-
-    for(let i = 0; i < 5; i++){
-        playRound(getComputerChoice(), getHumanChoice())
-        console.log("Score is: player: " + global.playerScore + ", computer: " + global.computerScore)
-    }
-
-    if(global.playerScore == global.computerScore){
-        console.log("It is a draw!")
-    } else if(global.playerScore > global.computerScore){
-        console.log("Congratulations! You have won!")
-    } else {
-        console.log("Computers are superior today. Better luck next time.")
-    }
+    rock.addEventListener("click", () => {playRound("rock")})
+    paper.addEventListener("click", () => {playRound("paper")})
+    scissors.addEventListener("click", () => {playRound("scissors")})
+    updateScore()
 }
 
 const getComputerChoice = () => {
@@ -44,27 +36,58 @@ const getHumanChoice = () => {
     return choices[humanChoice-1]
 }
 
-const playRound = (computerChoice, humanChoice) => {
+const playRound = (humanChoice) => {
+    let computerChoice = getComputerChoice();
+    let resultDiv = document.getElementById("result")
     if(computerChoice == humanChoice){
+        resultDiv.textContent = "Draw! No points!"
         console.log("Draw! No points!");
     } else if (computerChoice == "rock" && humanChoice == "paper"){
         global.playerScore++;
+        resultDiv.textContent = "You Win! Paper beats Rock!"
         console.log("You Win! Paper beats Rock!")
     } else if (computerChoice == "rock" && humanChoice == "scissors"){
         global.computerScore++;
+        resultDiv.textContent = "You Lose! Rock beats Scissors!"
         console.log("You Lose! Rock beats Scissors!")
     } else if (computerChoice == "paper" && humanChoice == "rock"){
         global.computerScore++;
+        resultDiv.textContent = "You Lose! Paper beats Rock!"
         console.log("You Lose! Paper beats Rock!")
     } else if (computerChoice == "paper" && humanChoice == "scissors"){
         global.playerScore++;
+        resultDiv.textContent = "You Win! Scissors beats Paper!"
         console.log("You Win! Scissors beats Paper!")
     } else if (computerChoice == "scissors" && humanChoice == "rock"){
         global.playerScore++;
+        resultDiv.textContent = "You Win! Rock beats Scissors!"
         console.log("You Win! Rock beats Scissors!")
     } else if (computerChoice == "scissors" && humanChoice == "paper"){
         console.log("You Lose! Scissors beats Paper!")
         global.computerScore++;
+        resultDiv.textContent = "You Lose! Scissors beats Paper!"
+    }
+    updateScore()
+}
+
+const updateScore = () => {
+    if(global.playerScore >= 5){
+        let scoreDiv = document.getElementById("score")
+        scoreDiv.textContent = `The score is: player: ${global.playerScore}, computer: ${global.computerScore}`
+        let resultDiv = document.getElementById("result")
+        resultDiv.textContent = "Congratulations! You have won the game!"
+        global.playerScore = 0;
+        global.computerScore = 0;
+    } else if(global.computerScore >= 5){
+        let scoreDiv = document.getElementById("score")
+        scoreDiv.textContent = `The score is: player: ${global.playerScore}, computer: ${global.computerScore}`
+        let resultDiv = document.getElementById("result")
+        resultDiv.textContent = "You have been defeated! Better luck next time!"
+        global.playerScore = 0;
+        global.computerScore = 0;
+    } else {
+        let scoreDiv = document.getElementById("score")
+        scoreDiv.textContent = `The score is: player: ${global.playerScore}, computer: ${global.computerScore}`
     }
 }
 
